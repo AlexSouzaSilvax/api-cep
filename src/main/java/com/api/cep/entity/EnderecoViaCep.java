028848834a94d0927750d8c8179cbaaffcb8579a
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2025-01-08 23:16:23 -0300

    testes endereco ViaCep

--- src/main/java/com/api/cep/entity/EnderecoViaCep.java
+++ src/main/java/com/api/cep/entity/EnderecoViaCep.java
@@ -1,10 +1,12 @@
 package com.api.cep.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class EnderecoViaCep {
 
     private String cep;
