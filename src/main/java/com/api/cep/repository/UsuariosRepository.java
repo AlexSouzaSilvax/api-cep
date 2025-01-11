026672ba051ea7a7129aaa78a633fe926c484619
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2025-01-11 05:59:06 -0300

    add existsByCpf

--- src/main/java/com/api/cep/repository/UsuariosRepository.java
+++ src/main/java/com/api/cep/repository/UsuariosRepository.java
@@ -1,9 +1,13 @@
 package com.api.cep.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.api.cep.entity.Usuarios;
 
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
-    
+
+    @Query(value = "SELECT EXISTS (SELECT 1 FROM Usuarios WHERE cpf = :pCpf ) AS registro_existe", nativeQuery = true)
+    boolean existsByCpf(@Param("pCpf") String pCpf);
 }
