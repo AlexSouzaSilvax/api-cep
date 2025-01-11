da6a0bd7fd39c54b5fe6a5ce4568c1784bf85fc4
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2025-01-11 06:27:52 -0300

    ajuste cors

--- src/main/java/com/api/cep/WebConfig.java
+++ src/main/java/com/api/cep/WebConfig.java
@@ -9,8 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("https://cep-app-1.onrender.com/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
